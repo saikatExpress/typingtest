20ee6da36d9c868b21ca899cc23edb39d54177ad
Saikat Talukder <saikathosen444@gmail.com>
2025-01-16 12:33:21 +0600

    ok

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -66,7 +66,7 @@ class WelcomeController extends Controller
 
     public function test()
     {
-        return 1100;
+        return 1100000;
     }
 
     public function calculateResult(Request $request)
